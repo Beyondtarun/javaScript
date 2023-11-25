54188a9a40707512ff475e8228a2f77618566831
Beyondtarun <teg.tarun@gmail.com>
2023-11-25 12:52:36 +0530

    updates

---
+++ variables.js
@@ -0,0 +1,31 @@
+/*
+1. Variables are containers to store some data or value.
+2. Null means empty not Zero
+3. Undefined means value is not defined.
+4. JavaScript is Dynammically typed. means specifiying datatype is not needed.
+5. Varaible names are case sensitive.
+6. Letter, digits , _ , $ is allowed only (no space)
+7. digits must not be first letter.
+8. cases:  camelCase , snake_case, kabab-case , PascalCase (js convention is to use camelCase)
+
+*/
+
+// const age = 24;
+let age = 24;
+// let age = 26; give me error because let wont allow to re declare.
+// age =23; give error when used age is declared with const.
+firstName = "Tarun";
+lastName= "Goyal";
+weight= 67.2;
+console.log(firstName, lastName, "Age is",age,"weight is",weight);
+
+/*
+let,const & var
+es6
+let->Variable cannot be redeclared but can be updated. A block scope variable.
+const->Variable cannot be redeclared or updated. A block scope variable.
+var->Variable can be redeclared & and updated. A global scope variable.
+
+*/
+
+
