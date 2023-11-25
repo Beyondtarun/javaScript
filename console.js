b9b4cba7fe6f94bddbe6c695cc667521128ae9d2
Beyondtarun <teg.tarun@gmail.com>
2023-11-25 12:08:41 +0530

    new addtion

---
+++ console.js
@@ -0,0 +1,5 @@
+/*
+1. Java Script is a programming language.
+*/
+console.log("welcome to console window "); //console.log(" ") used to print on screen anything between quotes
+console.log("Course by Tarun Goyal");
--- first.js
+++
@@ -1,1 +0,0 @@
-dnofowa,x a,
