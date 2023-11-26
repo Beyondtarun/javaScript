ccf6d2e729bf7919369d479c32d2fb95d1730a0c
Beyondtarun <teg.tarun@gmail.com>
2023-11-26 22:20:50 +0530

    update

--- console.js
+++ console.js
@@ -3,3 +3,4 @@
 */
 console.log("welcome to console window "); //console.log(" ") used to print on screen anything between quotes
 console.log("Course by Tarun Goyal");
+console.log("i am learning");
