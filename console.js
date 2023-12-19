69350c255be43f3820bc673f8e224ac204a3f122
Beyondtarun <teg.tarun@gmail.com>
2023-12-20 00:03:07 +0530

    update

--- console.js
+++ console.js
@@ -1,5 +1,6 @@
 /*
 1. Java Script is a programming language.
+2. Alert(display messages) and Prompt(input a number)
 */
 console.log("welcome to console window "); //console.log(" ") used to print on screen anything between quotes
 console.log("Course by Tarun Goyal");
