8003697329e3ccc3fc99ccec6ef55caa67556da6
Beyondtarun <teg.tarun@gmail.com>
2023-12-19 23:53:40 +0530

    update

--- conditional.js
+++ conditional.js
@@ -18,4 +18,8 @@ console.log(2+2+"2"+2+2+2)//42222
 CONDITIONAL STATEMENTS AND LOOPS
 IF-ELSE
 SWITCH
+TERNARY OPERATOR  CONDITION ? TRUE OUTPUT : FALSE OUTPUT
 */
+
+console.log(5>13 ? "true":"false")//teranary operator
+
