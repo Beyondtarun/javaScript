740e4b7adbd877425a517346cf47f063cab985b2
Beyondtarun <teg.tarun@gmail.com>
2023-12-19 23:56:25 +0530

    updates

--- conditional.js
+++ conditional.js
@@ -4,15 +4,14 @@ exponent (**)
 a**b
 uniary operator ++,--
 */
-console.log(8**2); //64
+console.log(8 ** 2); //64
 
-
-let a="5";
-let b=5; 
-console.log(a==b) //javascript implicitly convert string into number for comparision
-console.log(typeof(a))
-console.log(a===b)
-console.log(2+2+"2"+2+2+2)//42222
+let a = "5";
+let b = 5;
+console.log(a == b); //javascript implicitly convert string into number for comparision
+console.log(typeof a);
+console.log(a === b);
+console.log(2 + 2 + "2" + 2 + 2 + 2); //42222
 
 /*
 CONDITIONAL STATEMENTS AND LOOPS
@@ -21,5 +20,4 @@ SWITCH
 TERNARY OPERATOR  CONDITION ? TRUE OUTPUT : FALSE OUTPUT
 */
 
-console.log(5>13 ? "true":"false")//teranary operator
-
+console.log(5 > 13 ? "true" : "false"); //teranary operator
