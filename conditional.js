5530fbd04b613e6eb6dec50beca5f800ca616cba
Beyondtarun <teg.tarun@gmail.com>
2023-12-19 23:36:21 +0530

    update

--- conditional.js
+++ conditional.js
@@ -2,9 +2,16 @@
 Arithmetic operators
 exponent (**)
 a**b
+uniary operator ++,--
 */
 console.log(8**2); //64
 /*
 CONDITIONAL STATEMENTS AND LOOPS
 */
 
+let a="5";
+let b=5; 
+console.log(a==b) //javascript implicitly convert string into number for comparision
+console.log(typeof(a))
+console.log(a===b)
+console.log(2+2+"2"+2+2+2)
