6fe9a52e192bb38417e487049051251c96e4a5e4
Beyondtarun <teg.tarun@gmail.com>
2023-12-22 00:08:08 +0530

    updates

--- loops&Strings.js
+++ loops&Strings.js
@@ -1,6 +1,18 @@
 /*
-LOOPS
+LOOPS:- To execute a piece of code n number of times.
 1. for loop
 2. while
-3. Do While
+3. Do While 
+do{
+    //logic
+}while(condition);
+
+
+4. For of loop // use for string
+   for(let var of string)
+5. 
 */
+let str ="tarun";
+for(let i of str){
+    console.log(i);
+}
