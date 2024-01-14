445393fecbc8a460cdcef14020307a92aab376c6
Beyondtarun <teg.tarun@gmail.com>
2024-01-14 22:17:25 +0530

    update

--- arrays.js
+++ arrays.js
@@ -1,4 +1,10 @@
 /*
-collection of similar items
-
+collection of similar items(we can add diffrent items)
+Array is also a object
 */
+
+let a = [10,20,30];
+
+console.log(a)
+console.log(a.length)
+console.log(typeof a)
