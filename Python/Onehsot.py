d3b5a6acb74fc8d823055461b23b5c316c9b1344
Beyondtarun <teg.tarun@gmail.com>
2024-01-23 22:41:25 +0530

    udpate

---
+++ Python/Onehsot.py
@@ -0,0 +1,38 @@
+# #display
+
+# print("new learning")
+# first_name = "Tarun"
+# last_name = "goyal"
+# age = 45
+# is_genius = False
+# print("name of the candidate is",first_name,last_name)
+# print(first_name,"age is ", age,"years old")
+# if is_genius:
+    
+#     print(first_name,"is genius.")
+# else :
+#     print(first_name,"is not genius.")
+
+# first_name = input("Enter your name:")
+# print("Hello",first_name)
+
+# old_age = input("enter age:") #take input as string
+# new_age = int(old_age)+2
+# print(new_age)
+
+# first = input("Enter first number:")
+# second = input("Enter Second number:")
+
+# sum = int(first) +int(second)
+# print("sum is",sum)
+
+#strings
+
+name = "tarun"
+print(name.upper())
+print(name.find('ru'))
+print(name.replace('tarun','goyal'))
+print('t' in name)
+
+
+
