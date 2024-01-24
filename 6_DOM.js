01639b03805920d594f76a775b5aeebc1e049c04
Beyondtarun <teg.tarun@gmail.com>
2024-01-24 21:43:45 +0530

    update

--- 6_DOM.js
+++ 6_DOM.js
@@ -1,5 +1,22 @@
 /**
  * Document Object Manipulation
+ * 
+ * WINDOW OBJECT
+ * autommatically created by browser 
+ * 
+ * What is DOM?
+ * html ko javascript se accses krne ka trika hai DOM
+ * window->object->html
+ * console.dir(window.do cument)//print properties of document
  */
 // alert("tarun")
 console.log("Print")
+window.console.log("print1")//same as console.log but wont work in node command
+console.log(window)
+console.log(document.body)//this print code of body
+console.dir(document.body)//this print propeties of body
+document.body.style.background='green';//change bgcolor to green
+document.body.childNodes[5].innerText="abcd";//chnage first child node of body 
+/**
+ * DOM Manipulation
+ */
