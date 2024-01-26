6fdc85942686b4b2980150747ea919d8332acd7a
Beyondtarun <teg.tarun@gmail.com>
2024-01-26 19:46:58 +0530

    events

--- 7_Event_listeners.js
+++ 7_Event_listeners.js
@@ -2,3 +2,21 @@
  * EVENTS IN JAVASCRIPT
  * 
  */
+let div = document.getElementsByClassName("box")
+div[0].style.height="100px"
+div[0].style.width="100px"
+div[0].style.color="white"
+div[0].style.backgroundColor="blue"
+div[0].innerText="div 1"
+
+console.dir(div)
+
+let button = document.querySelectorAll("button")
+button[0].onclick =()=>{
+    console.log("hello world")
+}
+button[1].ondblclick=()=>{
+    console.log("dboule clicke!")
+}
+
+
