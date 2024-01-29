083cfb99307cc2d89547cd65a69682d490f47412
Beyondtarun <teg.tarun@gmail.com>
2024-01-29 13:40:57 +0530

    modified

--- toogle/script.js
+++ toogle/script.js
@@ -1,27 +1,28 @@
-let btn = document.getElementById("toggle")
-let body = document.getElementsByTagName("body")
-btn.innerText="Click Me!"
-btn.style.borderRadius="10px"
-let h1 = document.createElement("h1")
-h1.innerText="Color Changer"
-let mode= true;
-body[0].prepend(h1)
-// const chnageColor = ()=>{
-//     if(mode === "true"){
-//     body[0].style.background="black"
-//     body[0].style.color="white"
-//     mode = "false"
-//     }
-//     else if(mode === "false"){
-//         body[0].style.background="white"
-//         body[0].style.color="black"
-//         mode="true"
-//     }
-// }
-const chnageColor=()=>{
+let btn = document.getElementById("toggle");
+let body = document.getElementsByTagName("body");
+btn.innerText = "Click Me!";
+btn.style.borderRadius = "10px";
 
-body[0].classList.toggle("dark",mode)
-body[0].classList.toggle("light",!mode)
-mode =!mode
-}
-btn.addEventListener("click",chnageColor)
+let h1 = document.createElement("h1");
+h1.innerText = "Color Changer";
+let mode = true;
+
+body[0].prepend(h1);
+
+const chnageColor = () => {
+  //     if(mode === "true"){
+  //     body[0].style.background="black"
+  //     body[0].style.color="white"
+  //     mode = "false"
+  //     }
+  //     else if(mode === "false"){
+  //         body[0].style.background="white"
+  //         body[0].style.color="black"
+  //         mode="true"
+  //     }
+  body[0].classList.toggle("dark", mode);
+  body[0].classList.toggle("light", !mode);
+  mode = !mode;
+};
+
+btn.addEventListener("click", chnageColor);
